2c2df97c81c2747118066c535aaad8c639ccefd2
timoabra <154293310+timoabra@users.noreply.github.com>
2024-02-16 18:34:26 -0600

    Update ML_Stock_Prediction.py

--- ML_Stock_Prediction.py
+++ ML_Stock_Prediction.py
@@ -2,7 +2,7 @@ import streamlit as st
 import yfinance as yf
 import pandas as pd
 import os
-from datetime import index.tz
+from datetime import tzinfo
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import precision_score
 
